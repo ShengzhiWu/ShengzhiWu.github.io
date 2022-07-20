8f90e0ffefb78c5e3bddc8cdd99e9a17d01d16a8
ShengzhiWu <82275678+ShengzhiWu@users.noreply.github.com>
2022-07-20 20:51:18 +0800

    Update index.js

--- render/index.js
+++ render/index.js
@@ -2,41 +2,59 @@
 const canvas = document.querySelector("#myCanvas");
 var ctx = canvas.getContext("2d");
 
+var x = 200, y = 200, r=100;
+
 function paint()
 {
     ctx.clearRect(0, 0, canvas.width, canvas.height);  // 清空画布
 
-    ctx.fillStyle = "blue";
-    ctx.fillRect(100, 100, 200, 200);
+    ctx.fillStyle = "black";
     ctx.beginPath();
-    ctx.arc(150, 150, 50, 0, Math.PI * 2, true);
-    ctx.stroke();
+    ctx.arc(x, y, r, 0, Math.PI * 2, true);
+    ctx.fill();
 }
 
+var movingDisk = false;
+var mouseStartX, mouseStartY;  // 鼠标开始操作时的坐标
+var valueStart1, valueStart2;  // 鼠标开始操作时相应属性的值
+
 window.addEventListener("mousedown", function(event){  // 按下
-    console.log("mousedown");
+    // console.log("mousedown");
+    var dx = event.x-x;
+    var dy = event.y-y;
+    if(dx*dx+dy*dy<=r*r)
+    {
+        mouseStartX  = event.x;
+        mouseStartY  = event.y;
+        valueStart1 = x;
+        valueStart2 = y;
+        movingDisk = true;
+    }
 })
 
 window.addEventListener("mouseup", function(event){  // 松开
     // console.log("mouseup")
-    
+    movingDisk = false;
 })
 
 window.addEventListener("mousemove", function(event){  // 移动
-    mouseCurrentX = event.x;
-    mouseCurrentY = event.y;
     // console.log("mousemove");
-    
+    if(movingDisk)
+    {
+        x = valueStart1+(event.x-mouseStartX);
+        y = valueStart2+(event.y-mouseStartY);
+        paint();
+    }
 })
 
 window.addEventListener("keydown", function(event){  // 键盘
-    console.log(event.key)
+    // console.log(event.key)
 
     
 });
 
 window.addEventListener("keyup", function(event){  // 键盘
-    console.log(event.key)
+    // console.log(event.key)
 
 });
 
